b9e6be33ce470ad7d0f8a2570597eeb304557017
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-11 18:50:43 -0300

    feat: create schema Prisma migrations

--- prisma/migrations/20230311214749_create_users/migration.sql
+++ prisma/migrations/20230311214749_create_users/migration.sql
@@ -3,6 +3,9 @@ CREATE TABLE "users" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
