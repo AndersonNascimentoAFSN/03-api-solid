a31b79ca9b1234e7fee24f7a43e76238d9b568c4
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-11 19:43:16 -0300

    feat: create route POST /users

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,31 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Anderson Nascimento',
-    email: 'anderson.nascimento@gmail.com.br',
-  },
+  const { email, name, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
+
+app.get('/users', async (request, reply) => {
+  const users = await prisma.user.findMany()
+
+  return reply.status(200).send({ users })
 })
