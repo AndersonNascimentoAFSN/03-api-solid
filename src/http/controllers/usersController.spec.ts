53c11e6c2ab6157d83048bb8384f5385c51cc9bb
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-24 00:12:34 -0300

    feat: add test e2e for create user

--- src/http/controllers/usersController.spec.ts
+++ src/http/controllers/usersController.spec.ts
@@ -1,5 +1,23 @@
-import { describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
 
-describe('User Controller', () => {
-  it.only('ok', () => {})
+describe('User Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it.only('should be able to create user', async () => {
+    const response = await request(app.server).post('/v1/users').send({
+      name: 'John doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
 })
