b977882f4a53150a21459f83949572a1199a246b
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-24 00:34:52 -0300

    feat: add test e2e profile

--- src/http/controllers/usersController.spec.ts
+++ src/http/controllers/usersController.spec.ts
@@ -20,4 +20,29 @@ describe('User Controller (e2e)', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it.only('should be able to get user profile', async () => {
+    await request(app.server).post('/v1/users').send({
+      name: 'John doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const authResponse = await request(app.server).post('/v1/sessions').send({
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/v1/profile')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({ email: 'john.doe@example.com' }),
+    )
+  })
 })
