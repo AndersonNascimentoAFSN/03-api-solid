74127a2ac27e0e43f6dbf5bf2257aeec7bd1c1fc
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-03-25 10:55:18 -0300

    fix: bug

--- src/http/controllers/users/usersRoutes.ts
+++ src/http/controllers/users/usersRoutes.ts
@@ -5,15 +5,14 @@ import { verifyJWT } from '../../middlewares/verify-jwt'
 export async function usersRoutes(app: FastifyInstance) {
   const usersController = makeUsersController()
 
-  app.post('/users', (req, reply) => usersController.registerUser(req, reply))
-
   /* Authenticated */
-  // app.addHook('onRequest', verifyJWT)
+  app.addHook('onRequest', verifyJWT)
 
-  app.get('/users', { onRequest: [verifyJWT] }, (req, reply) =>
-    usersController.listUsers(req, reply),
-  )
-  app.get('/profile', { onRequest: [verifyJWT] }, (req, reply) =>
-    usersController.profile(req, reply),
-  )
+  app.get('/users', (req, reply) => usersController.listUsers(req, reply))
+
+  app.get('/profile', (req, reply) => usersController.profile(req, reply))
+
+  // app.get('/profile', { onRequest: [verifyJWT] }, (req, reply) =>
+  //   usersController.profile(req, reply),
+  // )
 }
